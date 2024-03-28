c277d63f4d8785dc45263744720a18f8f01fbb92
Air2air <todd@dunningmarketing.com>
2024-03-27 19:23:32 -0700

    Add new files and update prompt.txt

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,16 @@
 import firebase from "firebase/app";
+
 import "firebase/auth"; // If you're using Firebase authentication
 import "firebase/firestore"; // If you're using Firebase Firestore
 // Add other Firebase services that you want to use
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyLPuUC5d1PZK4W1OLIdpLQXPXCHr_kPI",
-  authDomain: "dothis-a2801.firebaseapp.com",
-  projectId: "dothis-a2801",
-  storageBucket: "dothis-a2801.appspot.com",
-  messagingSenderId: "474233975978",
-  appId: "1:474233975978:web:e458c247db36b94a5fccd3",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
